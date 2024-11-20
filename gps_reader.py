66bfd3a7e3170bfb40168d408aa6e9e6168ab6c3
JacobErwinOSU <112585184+JacobErwinOSU@users.noreply.github.com>
2024-11-19 21:16:14 -0600

    Update gps_reader.py

--- gps_reader.py
+++ gps_reader.py
@@ -10,8 +10,8 @@ try:
         #print("Raw GPS Report:")
         #print(report)
 
+        # Check if the report contains position data (class 'TPV')
         if report['class'] == 'TPV':
-
             latitude = report.lat if hasattr(report, 'lat') else 'No Data'
             longitude = report.lon if hasattr(report, 'lon') else 'No Data'
             elevation = report.altMSL if hasattr(report, 'altMSL') else 'No Data'
@@ -20,15 +20,18 @@ try:
             print(f"Longitude: {longitude}")
             print(f"Elevation: {elevation} meters")
 
-        elif report['class'] == 'GSV':
+        # Check if the report contains satellite data (class 'SKY')
+        elif report['class'] == 'SKY':
+            nSat = report.nSat if hasattr(report, 'nSat') else 'No Data'  # Number of locked satellites
+
+            print(f"Number of locked satellites: {nSat}")
             satellites = report.satellites if hasattr(report, 'satellites') else 'No Data'
 
-            print(f"Satellites: {satellites}")
-        
+
         print("===================================")
         time.sleep(1)
 
 except KeyboardInterrupt:
     print("Program interrupted")
-except Excption as e:
+except Exception as e:
     print(f"Error: {e}")
